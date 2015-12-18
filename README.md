c9201c860fcf62a47f25ad84f1a87c9eaf831e47
Maximiliano Castro <macguionbajo@gmail.com>
2015-12-18 01:32:10 -0300

    Refinado parseo de argumentos

--- README.md
+++ README.md
@@ -16,3 +16,11 @@ La automatización cubre:
 El hardware adicional documentado acá involucra:
   - Circuito para relés
   - Panel de status y de conexión de dispositivos
+
+TODO:
+    - Capacidad de definir hora de encendido, hora de apagado, factor de aleatorización para dispositivos, con servidor ya inicializado
+    - Capacidad de encender y apagar dispositivos
+    - Integrar como servicio linux en init.d
+    - Interfaz gráfica sencilla
+    - Capacidad de setear característica Override-pin-state (si debiera tener un estado, pero no lo tiene (algo lo cambió por otro lado), forzarlo)
+    - Implementar mayor flexibilidad para fijar horarios, tipo crontab
--- SOFTWARE/domoclient.py
+++ SOFTWARE/domoclient.py
@@ -42,8 +42,12 @@ ROJO_CLARO='\033[1;31m'
 MORADO_CLARO='\033[1;35m'
 AMARILLO='\033[1;33m'
 BLANCO='\033[1;37m'
-
 NO_COLOR='\033[0m'
+
+# Variables para conexión con el servidor
+HOSTS = ["","192.168.0.3","mexomagno.duckdns.org"] # Localhost, raspi local, raspi remota
+PORT = 8004 # Definido por mi
+
 def repeatChar(c,ntimes):
     return c*ntimes
 def drawTop():
@@ -104,7 +108,7 @@ def showHelp(mensaje):
         absent_mode    ["on | enabled | 1", "off | disabled | 0"] :
                 Habilita o deshabilita el modo "Nadie en casa". En este modo, el sistema simula movimiento normal en el hogar. 
         
-        help :
+        -h, --help :
                 Muestra esta ayuda
     """
     if mensaje != "":
@@ -113,36 +117,86 @@ def showHelp(mensaje):
     exit()
 # Definición de argumentos válidos
 valid_args={    'absent_mode'   : ['on','off','enabled','disabled','1','0'],
-                'status'        : ["",'json']}
-def validarArgumentos():
-    import sys
-    # Obtener argumentos y validarlos
-    args=sys.argv
-    nargs=len(args) - 1
-    if nargs == 0:
-        # comportamiento default
-        command = "status"
-    else:
-        command = args[1]
-    # Ver que el argumento es válido
-    if command not in valid_args:
-        showHelp("Argumento inválido: '{}'".format(command))
-    # Obtener propiedad para el argumento
-    if nargs == 1:
-        if "" not in valid_args[command]:
-            showHelp("Falta parámetro para '{}'".format(command))
-    if nargs == 2:
-        if args[2] not in valid_args[command]:
-            showHelp("Parámetro desconocido para '{}'".format(command))
-        command = command + " " + args[2]
-    if nargs >2:
-        showHelp("Demasiados argumentos")
-    return command
+                'status'        : ["",'json'],
+                'help'          : ["-h", "--help", "-?"]}
+def panic(mensaje, parser):
+    print ROJO_CLARO + mensaje + NO_COLOR
+    parser.print_help()
+    exit(1)
 
+def parseArguments():
+    # import sys
+    # # Obtener argumentos y validarlos
+    # args=sys.argv
+    # nargs=len(args) - 1
+    # if nargs == 0:
+    #     # comportamiento default
+    #     command = "status"
+    # else:
+    #     command = args[1]
+    # # Ver que el argumento es válido
+    # if command not in valid_args:
+    #     showHelp("Argumento inválido: '{}'".format(command))
+    # # Obtener propiedad para el argumento
+    # if nargs == 1:
+    #     if "" not in valid_args[command]:
+    #         showHelp("Falta parámetro para '{}'".format(command))
+    # if nargs == 2:
+    #     if args[2] not in valid_args[command]:
+    #         showHelp("Parámetro desconocido para '{}'".format(command))
+    #     command = command + " " + args[2]
+    # if nargs >2:
+    #     showHelp("Demasiados argumentos")
+    # return command
+
+    import argparse as AP
+    parser = AP.ArgumentParser(description="Comunicación con servidor de domótica", epilog="Versión 1.1")
+    on_off_choices = ["on", "off", "enabled", "disabled", "1", "0", "true", "false"]
+    gpio_choices = ["2","3","4","17","18","15","14"]
+    
+    parser.add_argument("-s", "--status", nargs="?", const="graphic", choices=["graphic", "json"], help="Obtener estado de los dispositivos. Este es el comportamiento default del programa, si se lo llama sin argumentos. Si se desea output en formato json, se debe además incluir STAT='json'.")
+    # Opciones configuración de un dispositivo
+    parser.add_argument("-d", "--disp", nargs=1, choices=gpio_choices, help="Elige un dispositivo a configurar")
+    parser.add_argument("-sv", "--set-value", nargs=1, choices=on_off_choices, help="Setea inmediatamente un estado para el dispositivo en cierto GPIO. Valor NO afectado por OVERRIDE.")
+    parser.add_argument("-on", "--on-time", nargs=2, type=int, metavar=("HH", "MM"), help="Setea hora de encendido del dispositivo.")
+    parser.add_argument("-off", "--off-time", nargs=2, type=int, metavar=("HH", "MM"), help="Setea hora de apagado del dispositivo.")
+    # Opciones globales para el servicio
+    parser.add_argument("-am", "--absent-mode", nargs=1, choices=on_off_choices, help="Activar|desactivar modo ausente. Utilizado cuando no hay nadie presente en la casa.")
+    parser.add_argument("--override-status", nargs=1, choices=on_off_choices, help="Setea funcionalidad OVERRIDE. Si está activada, el servidor sobreescribirá output del pin si su estado ha sido cambiado por otro proceso. En caso contrario, se mantiene última modificación al output.")
+    # Opciones de conexión con el servidor
+    parser.add_argument("-H", "--host", nargs="+", help="Lista de hosts a los que intentar conectarse. Por defecto, se usa '{}'".format(HOSTS))
+    parser.add_argument("-P", "--port", nargs=1, type=int, help="Puerto del host. Por defecto, se usa el '{}'".format(PORT))
+    args = parser.parse_args()
+
+    # PARSEAR ARGUMENTOS
+    argsdict = vars(args)
+    # Si todos son None, se asume status=graphic
+    allnone = True
+    for arg in argsdict:
+        if argsdict[arg] != None:
+            allnone = False
+            break
+    if allnone:
+        return "status"
+    # opciones para dispositivo NO pueden especificarse sin especificar qué dispositivo se quiere configurar
+    if argsdict["disp"] == None:
+        if argsdict["set_value"] != None or argsdict["on_time"] != None or argsdict["off_time"] != None:
+            panic("Error: Debe especificar un dispositivo al cual asignar esta opción!", parser)
+    # Horas ingresadas deben ser válidas
+    if argsdict["on_time"] != None and (argsdict["on_time"][0] not in range(24) or argsdict["on_time"][1] not in range(60)):
+        panic("Error: La hora de encendido '{}:{}' no es válida".format(argsdict["on_time"][0], argsdict["on_time"][1]), parser)
+    if argsdict["off_time"] != None and (argsdict["off_time"][0] not in range(24) or argsdict["off_time"][1] not in range(60)):
+        panic("Error: La hora de apagado '{}:{}' no es válida".format(argsdict["off_time"][0], argsdict["off_time"][1]), parser)
+    print argsdict
+    # Puerto debe ser válido
+    if argsdict["port"] != None and argsdict["port"][0] < 1024:
+        panic("Error: El nro de puerto debe ser mayor a 1024", parser)
+    
+    return argsdict  
 ##############################################################
 # Conexión y funcionamiento general
 ##############################################################
-def atender(command, reply):
+def serve(command, reply):
     c_a = command.split()
     if c_a[0] == "absent_mode":
         if reply == "OK":
@@ -156,15 +210,13 @@ def atender(command, reply):
         else:
             print reply
 
-def conectar():
+def connect():
     import socket
-    hosts = ["","192.168.0.3","mexomagno.duckdns.org"] # Localhost, raspi local, raspi remota
-    port = 8004 # Definido por mi
-    for host in hosts:
+    for host in HOSTS:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try: 
             s.settimeout(TIMEOUT)
-            s.connect((host,port))
+            s.connect((host,PORT))
             s.settimeout(None)
             return s
         except socket.error, msg:
@@ -178,14 +230,14 @@ def conectar():
 # MAIN
 ###########################################################################
 def main():
-    command = validarArgumentos()
+    command = parseArguments()
     #import socket
     #import yaml
     from subprocess import call
-    s = conectar()
+    s = connect()
     s.send(command)
     reply = s.recv(1024)
-    atender(command, reply)
+    serve(command, reply)
     s.close()
 
 main()
@@ -195,8 +247,21 @@ main()
 PENDIENTE:
 - [DONE] independizarse del archivo en C
 - Posibilidad de mostrar horas de inicio y término, y threshold random seteado
-- Mejorar parseo de opciones y usar mejores estándares
-- Añadir autocompletado a opciones
+- [DONE]Mejorar parseo de opciones y usar mejores estándares
+- [NOT]Añadir autocompletado a opciones
 - Posibilidad de especificar host y puerto
+
+"""
+
+"""
+De qué distintas formas se puede definir horarios de encendido y apagado para un dispositivo?
+
+    - Una forma es definir un horario y repetirlo todos los días
+        - Una extensión es permitir varios horarios para un día y repetirlos
+    - Otra forma, especificar repeticiones como crontab, es decir "m h dom mon dow"
+
+Cuál debiera ser el resultado para tener correctamente especificado un horario?
+    - Debiera tener al menos una hora específica de inicio y término. Puede tener varias
+    
 
 """
